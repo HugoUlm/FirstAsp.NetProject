73a67c46d96fcb5a1b4657fe0f9652797ebe4ef6
S182730 <s182730@student.hb.se>
2021-11-22 23:25:18 +0100

    Add project files.

---
+++ E-Commerce/wwwroot/js/slideshows.js
@@ -0,0 +1,14 @@
+﻿var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
