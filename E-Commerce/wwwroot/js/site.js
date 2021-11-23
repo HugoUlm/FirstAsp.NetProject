ae86219c8f53b5b716aba7888ca8f61558ed89c6
S182730 <s182730@student.hb.se>
2021-11-23 01:18:40 +0100

    navbar-styling etc.

--- E-Commerce/wwwroot/js/site.js
+++ E-Commerce/wwwroot/js/site.js
@@ -3,3 +3,14 @@
 
 // Write your JavaScript code.
 
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar").style.top = "0";
+    } else {
+        document.querySelector(".navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
