c26459db9057387f393470ad9420ed80d5744f93
S182730 <s182730@student.hb.se>
2021-11-25 22:55:00 +0100

    Homepage, navbar, animations

--- E-Commerce/wwwroot/js/site.js
+++ E-Commerce/wwwroot/js/site.js
@@ -3,14 +3,49 @@
 
 // Write your JavaScript code.
 
+// When the user scrolls down, navbar dissapears
 var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function () {
+window.addEventListener("scroll", function () {
+
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
+    if (prevScrollpos > currentScrollPos && currentScrollPos == 0) {
         document.querySelector(".navbar").style.top = "0";
-    } else {
+    }
+    else {
         document.querySelector(".navbar").style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
-}
+});
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+var prevScrollposi = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollposi > currentScrollPos && currentScrollPos == 0) {
+        document.getElementById("header").style.fontSize = "24px";
+        document.getElementById("header").style.position = "fixed";
+        document.getElementById("header").style.textAlign = "left";
+        document.getElementById("header").style.top = "70px";
+        document.getElementById("header").style.padding = "0px";
+        document.getElementById("header").style.marginLeft = "0%";
+        document.getElementById("container").style.marginTop = "110px";
+    }
+    else {
+        document.getElementById("header").style.fontSize = "18px";
+        document.getElementById("header").style.position = "fixed";
+        document.getElementById("header").style.textAlign = "left";
+        document.getElementById("header").style.top = "0px";
+        document.getElementById("header").style.padding = "10px 20px";
+        document.getElementById("header").style.marginLeft = "-15%";
+        document.getElementById("container").style.marginTop = "0px";
+    }
+    if (currentScrollPos > 0) {
+        document.getElementById("header").style.backgroundColor = "white";
+    }
+    if (currentScrollPos <= 0) {
+        document.getElementById("header").style.backgroundColor = "transparent";
+    }
+    prevScrollposi = currentScrollPos;
+});
