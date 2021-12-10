8ec4ea9ba3275c258d30beb8330d0a260a09c319
S182730 <s182730@student.hb.se>
2021-12-11 00:41:48 +0100

    Added models for cart

---
+++ E-Commerce/Models/ShoppingCartItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce.Models
+{
+    public class ShoppingCartItemViewModel
+    {
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public byte[] Images { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string ProductNo { get; set; }
+    }
+}
---
+++ E-Commerce/Models/ShoppingCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Models
+{
+    public class ShoppingCartViewModel
+    {
+        public int? Id { get; set; }
+        public List<ShoppingCartItemViewModel> Items { get; set; }
+    }
+}
